f9be6b8d0969ac91510c751d9037cb2a043f04af
Bjorn le Fevre <doggiehostmaster@gmail.com>
2014-01-19 16:58:33 +0100

    Test project and test code

--- DoggieCreationsFramework/Class1.cs
+++ DoggieCreationsFramework/Class1.cs
@@ -1,11 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 
 namespace DoggieCreationsFramework
 {
-    public class Class1
+    public static class DcString
     {
+        public static string Formatteer(this string @string)
+        {
+            var keyValuePair = DcType<string>.Formatteer(@string);
+            Logging.Add(keyValuePair);
+            return keyValuePair.Key;
+        }
+    }
+
+    public static class DcDictionary
+    {
+        public static void Add<TKey, TValue>(this Dictionary<TKey, TValue> @dic, KeyValuePair<TKey, TValue> keyValuePair)
+        {
+            if (@dic == null) @dic = new Dictionary<TKey, TValue> {{keyValuePair.Key, keyValuePair.Value}};
+            if (@dic.ContainsKey(keyValuePair.Key)) @dic[keyValuePair.Key] = keyValuePair.Value;
+            else @dic.Add(keyValuePair.Key, keyValuePair.Value);
+        }
+    }
+
+    public class DcType<T> : DoggieCreationsLogger<T>
+    {
+        public static KeyValuePair<T, ApplicationException> Formatteer(T @t)
+        {
+            return new KeyValuePair<T, ApplicationException>(@t, LogShit<T>("Damn"));
+        }
+    }
+
+    public abstract class DoggieCreationsLogger<T> : IDoggieCreationsLogger<T>
+    {
+        public Collection<IDoggieCreationsLogger<T>> Loggers;
+
+        public static ApplicationException LogShit<T>(string message)
+        {
+            return new ApplicationException(message);
+        }
+
+        public delegate void AddLogging(T @t);
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public abstract class DoggieCreationsUnitTestLogger<T> : IDoggieCreationsLogger<T>
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IDoggieCreationsLogger<T> : IDisposable
+    {
+        
     }
 }
---
+++ DoggieCreationsUnitTest/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using DoggieCreationsFramework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DoggieCreationsUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var result = "asf{0}".Formatteer();
+            var applicationExceptions = DcString.Logging;
+            var message = applicationExceptions[result].Message;
+            Assert.AreEqual("Damn", message);
+        }
+    }
+}
