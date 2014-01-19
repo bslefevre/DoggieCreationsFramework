0c9d924548ab5039a6b5d67d0e2223c8ae145515
Bjorn le Fevre <doggiehostmaster@gmail.com>
2014-01-19 17:54:09 +0100

    Added base

--- DoggieCreationsFramework/Class1.cs
+++ DoggieCreationsFramework/Class1.cs
@@ -25,43 +25,87 @@ namespace DoggieCreationsFramework
             if (@dic.ContainsKey(keyValuePair.Key)) @dic[keyValuePair.Key] = keyValuePair.Value;
             else @dic.Add(keyValuePair.Key, keyValuePair.Value);
         }
-    }
 
-    public class DcType<T> : DoggieCreationsLogger<T>
-    {
-        public static KeyValuePair<T, ApplicationException> Formatteer(T @t)
+        public static bool IsEmpty<TKey, TValue>(this Dictionary<TKey, TValue> @dic)
         {
-            return new KeyValuePair<T, ApplicationException>(@t, LogShit<T>("Damn"));
+            if (@dic == null) return true;
+            return !@dic.Any();
         }
     }
 
-    public abstract class DoggieCreationsLogger<T> : IDoggieCreationsLogger<T>
+    public class DcType<T> : FrameworkBase
     {
-        public Collection<IDoggieCreationsLogger<T>> Loggers;
+        public static KeyValuePair<T, ApplicationException> Formatteer(T @t)
+        {
+
+            AddLogging();
+            return new KeyValuePair<T, ApplicationException>(@t, null);
+        }
+    }
+
+    public class FrameworkBase
+    {
+        private static Dictionary<LoggerType, IDoggieCreationsLogger> _loggers; 
+        public static Dictionary<LoggerType, IDoggieCreationsLogger> Loggers
+        {
+            get
+            {
+                if (_loggers.IsEmpty())
+                {
+                    _loggers.Add(new KeyValuePair<LoggerType, IDoggieCreationsLogger>(LoggerType.UnitTest, new DoggieCreationsUnitTestLogger()));
+                    _loggers.Add(LoggerType.Live, new DoggieCreationsLogger());
+                }
+                return _loggers;
+            }
+        }
+
+        public static void AddLogging()
+        {
+            Loggers[LoggerType.UnitTest].AddLogging();
+        }
+    }
+
+    public enum LoggerType
+    {
+        UnitTest,
+        Live
+    }
+
+    public class DoggieCreationsLogger : IDoggieCreationsLogger
+    {
+        public Collection<IDoggieCreationsLogger> Loggers;
 
         public static ApplicationException LogShit<T>(string message)
         {
             return new ApplicationException(message);
         }
 
-        public delegate void AddLogging(T @t);
-
         public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddLogging()
         {
             throw new NotImplementedException();
         }
     }
 
-    public abstract class DoggieCreationsUnitTestLogger<T> : IDoggieCreationsLogger<T>
+    public class DoggieCreationsUnitTestLogger : IDoggieCreationsLogger
     {
         public void Dispose()
         {
             throw new NotImplementedException();
         }
+
+        public void AddLogging()
+        {
+            throw new NotImplementedException();
+        }
     }
 
-    public interface IDoggieCreationsLogger<T> : IDisposable
+    public interface IDoggieCreationsLogger : IDisposable
     {
-        
+        void AddLogging();
     }
 }
