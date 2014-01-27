6c4184a1efaa88672d0e912b76bc7b293d667480
Bjorn le Fevre <doggiehostmaster@gmail.com>
2014-01-27 16:45:45 +0100

    Create Resource toegevoegd.
    Wellicht handig voor de multi lang.

--- DoggieCreationsFramework/DcHelpers.cs
+++ DoggieCreationsFramework/DcHelpers.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
 using System.Reflection.Emit;
+using System.Resources;
 using System.Windows.Forms;
 
 namespace DoggieCreationsFramework
@@ -28,6 +33,42 @@ namespace DoggieCreationsFramework
             }
 
             return @string;
+        }
+
+        /// <summary>
+        /// To split the string in pieces by NewRule and end of a sentence.
+        /// </summary>
+        /// <param name="text">List of sentences to translate.</param>
+        /// <returns></returns>
+        public static IEnumerable<string> SplitString(string text)
+        {
+            var splittedStringList = new List<string>();
+
+            var splittedTextArray = text.Split('\n');
+
+            foreach (var s in splittedTextArray)
+            {
+                var nr = 0;
+                while (s.IndexOf(".", nr) > -1 || s.IndexOf(".\r", nr) > -1)
+                {
+                    var ir = s.IndexOf(".\r", nr);
+                    var ip = s.IndexOf(".", nr);
+                    var i = 0;
+                    if (ip < ir || ir == -1)
+                        i = ip + 1;
+                    else if (ip == ir && ir > -1)
+                        i = ir + 2;
+                    else if (ir > -1)
+                        i = ir + 2;
+                    splittedStringList.Add(s.Substring(nr, i - nr));
+                    nr = i;
+                }
+
+                if (!string.IsNullOrEmpty(s.Substring(nr, s.Length - nr)))
+                    splittedStringList.Add(s.Substring(nr, s.Length - nr));
+            }
+
+            return splittedStringList;
         }
     }
 
@@ -110,7 +151,7 @@ namespace DoggieCreationsFramework
 
     public static class DcControl
     {
-        public static void SetFullWidthToAllControls(this System.Windows.Forms.Control.ControlCollection controlCollection)
+        public static void SetFullWidthToAllControls(this Control.ControlCollection controlCollection)
         {
             var ownerControl = controlCollection.Owner;
             var maxWidth = ownerControl.Parent != null ? ownerControl.Parent.Width : (int?)null;
@@ -124,7 +165,7 @@ namespace DoggieCreationsFramework
                 control.Width = maxWidth.Value - control.Margin.Horizontal;
             }
         }
-        public static void AddControl(this System.Windows.Forms.Control.ControlCollection controlCollection, Control controlToAdd)
+        public static void AddControl(this Control.ControlCollection controlCollection, Control controlToAdd)
         {
             var combinedHeight = 0;
             foreach (var control in controlCollection.Cast<Control>())
@@ -133,6 +174,89 @@ namespace DoggieCreationsFramework
             }
             controlToAdd.Location = new Point(controlToAdd.Location.X, combinedHeight);
             controlCollection.Add(controlToAdd);
+        }
+    }
+
+    public class DcResource
+    {
+        public class Res
+        {
+            public string TypeName { get; set; }
+            public string OriginalResourcePath { get; set; }
+            public bool OriginalResourceFileExists { get { return File.Exists(OriginalResourcePath); } }
+            public string NewResourcePath { get; set; }
+            public bool NewResourceFileExists(string language) { return File.Exists(string.Format(NewResourcePath, language.ToUpper())); }
+        }
+
+        private Collection<Res> GetAllResourceFilesWithinAssembly<T>(string baseFolder)
+        {
+            var assembly = Assembly.GetAssembly(typeof(T));
+            var strippedAssembly = assembly.ManifestModule.Name.Remove(assembly.ManifestModule.Name.Length - 4);
+            var resourceCollection = new Collection<Res>();
+
+            foreach (var manifestResourceName in assembly.GetManifestResourceNames())
+            {
+                var resourceName =
+                    manifestResourceName.Remove(manifestResourceName.Length - 10)
+                                        .Replace(string.Format("{0}.", strippedAssembly), string.Empty);
+                resourceCollection.Add(new Res
+                {
+                    TypeName = resourceName,
+                    OriginalResourcePath = GetFullResourcePath(baseFolder, strippedAssembly, resourceName),
+                    NewResourcePath = GetFullResourcePathWithLanguage(baseFolder, strippedAssembly, resourceName)
+                });
+            }
+            return resourceCollection;
+        }
+
+        private static string GetFullResourcePath(string baseFolder, string assembly, string resourceName)
+        {
+            return string.Format(@"{0}{1}\{2}.resx", baseFolder, assembly, resourceName.Replace(".", @"\"));
+        }
+
+        private static string GetFullResourcePathWithLanguage(string baseFolder, string assembly, string resourceName)
+        {
+            return string.Format(@"{0}{1}\{2}{3}", baseFolder, assembly, resourceName.Replace(".", @"\"), ".{0}.resx");
+        }
+
+        private static void CreateResourceFile(Res res, string toLanguage)
+        {
+            if (!res.OriginalResourceFileExists) return;
+            var rrw = new ResXResourceReader(res.OriginalResourcePath);
+            if (res.OriginalResourcePath.Contains("Properties"))
+            {
+                //rrw.BasePath = @"C:\AlureOntwikkelingDev12\Alure.WS.BL\Resources";
+            }
+
+            var fileStream = File.Open(string.Format(res.NewResourcePath, toLanguage.ToUpper()), FileMode.OpenOrCreate);
+            using (var resXResourceWriter = new ResXResourceWriter(fileStream))
+            {
+                try
+                {
+                    foreach (var dictionary in rrw.OfType<DictionaryEntry>())
+                    {
+                        var translatedValue = string.Empty;
+                        if (!string.IsNullOrEmpty(dictionary.Value.ToString()))
+                        {
+                            var splittedStringList = DcString.SplitString(dictionary.Value.ToString());
+                            foreach (var @string in splittedStringList.Where(x => !string.IsNullOrEmpty(x)))
+                            {
+                                var trimmedString = @string.Trim();
+                                trimmedString = TranslateClass.Translate(trimmedString, "nl", toLanguage.ToLower());
+                                if (@string.EndsWith("\r"))
+                                    trimmedString += Environment.NewLine;
+                                translatedValue += trimmedString;
+                            }
+                        }
+                        if (string.IsNullOrEmpty(translatedValue))
+                            translatedValue = string.Format("-TODO-{0}", dictionary.Value);
+                        resXResourceWriter.AddResource(dictionary.Key.ToString(), translatedValue);
+                    }
+                }
+                catch (Exception e)
+                {
+                }
+            }
         }
     }
 }
